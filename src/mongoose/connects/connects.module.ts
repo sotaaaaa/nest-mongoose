ac7990fa4b71f51cbf5e12a3ed84c3ec4316da6a
Sotaaaaa <dungla2708@gmail.com>
2022-12-06 23:18:43 +0700

    Feat: Version 1.0.5

--- src/mongoose/connects/connects.module.ts
+++ src/mongoose/connects/connects.module.ts
@@ -20,7 +20,7 @@ export class MongooseConnectsModule {
   static forRootAsync(): DynamicModule {
     // const maxConnects = new Array(50).fill(0);
     const imports = [];
-    console.log(this.configService.get('database'), 'TEST MODULE');
+    // console.log(this.configService.get('database'), 'TEST MODULE');
 
     /**
      * Quy định tối đa chỉ được 50 connection
